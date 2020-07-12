b14070d138986a4a67c0a1f56c9221918b0b45ac
ChrisChrisLoLo <sporewoh@gmail.com>
2020-07-12 17:50:05 -0600

    remove fbs

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,22 @@
-from fbs_runtime.application_context.PySide2 import ApplicationContext
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton
 
 import sys
+from PySide2.QtWidgets import QApplication
+from PySide2.QtQuick import QQuickView
+from PySide2.QtCore import QUrl, Slot
+
+
+@Slot()
+def say_hello():
+    print("Button clicked, Hello!")
+
 
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    sys.exit(exit_code)
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create a button, connect it and show it
+    button = QPushButton("Click me")
+    button.clicked.connect(say_hello)
+    button.show()
+    # Run the main Qt loop
+    app.exec_()
